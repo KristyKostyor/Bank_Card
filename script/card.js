12063df828a60079407fc6d2f1b9a4b6b7adf4de
KristyKostyor <ms.kostyor@gmail.com>
2024-01-22 18:05:22 +0300

    init

---
+++ script/card.js
@@ -0,0 +1,186 @@
+export const Card = () => {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("wrapper");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const secure = document.createElement("p");
+  secure.classList.add("secure");
+  secure.textContent = "Secure Checkout";
+
+  const creditCard = document.createElement("div");
+  creditCard.classList.add("credit-card");
+
+  const cardNumber = document.createElement("span");
+  cardNumber.classList.add("card__number");
+  cardNumber.textContent = "xxxx xxxx xxxx xxxx";
+
+  const cardPersonal = document.createElement("div");
+  cardPersonal.classList.add("card__personal");
+
+  const cardName = document.createElement("span");
+  cardName.classList.add("card__name");
+  cardName.textContent = "John Doe";
+  const cardDate = document.createElement("span");
+  cardDate.classList.add("card__date");
+  cardDate.textContent = "04/24";
+
+  cardPersonal.append(cardName, cardDate);
+
+  creditCard.append(cardNumber, cardPersonal);
+
+  const form = document.createElement("form");
+  form.classList.add("form");
+  form.setAttribute("id", "form");
+
+  // держатель карты
+  const formInputHolder = document.createElement("div");
+  formInputHolder.classList.add("form__input-wrap", "form__input-wrap_holder");
+
+  const holderLabel = document.createElement("label");
+  holderLabel.classList.add("form__label", "form__holder-label");
+  holderLabel.textContent = "Card Holder";
+
+  const holder = document.createElement("input");
+  holder.classList.add("input", "input__holder");
+  holder.type = "text";
+  holder.name = "holder";
+  holder.required = true;
+  // holder.pattern = "[A-Za-z\s]+$";
+  formInputHolder.append(holderLabel, holder);
+
+  holder.addEventListener("input", () => {
+    holder.value = holder.value.replace(/[^A-Za-z\s]+$/gi, "").toUpperCase();
+    cardName.textContent = holder.value.toUpperCase();
+  });
+
+  const mask = (value, limit, separator) => {
+    let output = [];
+    for (let i = 0; i < value.length; i++) {
+      if (i !== 0 && i % limit === 0) {
+        output.push(separator);
+      }
+      output.push(value[i]);
+    }
+    return output.join("");
+  };
+
+  // номер карты
+  const formInputNumber = document.createElement("div");
+  formInputNumber.classList.add("form__input-wrap", "form__input-wrap_number");
+
+  const numberLabel = document.createElement("label");
+  numberLabel.classList.add("form__label", "form__number-label");
+  numberLabel.textContent = "Card Number";
+
+  const number = document.createElement("input");
+  number.classList.add("input", "input__number");
+  number.type = "text";
+  number.name = "number";
+  number.setAttribute("id", "cardNumber");
+  number.pattern = "[0-9]";
+  number.title = "enter 16 numbers";
+  number.setAttribute("maxlength", 19);
+  number.required = true;
+  formInputNumber.append(numberLabel, number);
+
+  number.addEventListener("input", () => {
+    number.value = number.value.replace(/[^0-9]/gi, "");
+    number.value = mask(number.value, 4, " ");
+    cardNumber.textContent = number.value;
+  });
+
+  // дата карты
+  const formInputDate = document.createElement("div");
+  formInputDate.classList.add("form__input-wrap", "form__input-wrap_date");
+
+  const dateLabel = document.createElement("label");
+  dateLabel.classList.add("form__label", "form__date-label");
+  dateLabel.textContent = "Card Expiry";
+
+  const date = document.createElement("input");
+  date.classList.add("input", "input__date");
+  date.type = "text";
+  date.pattern = /^\d{0,4}$/g;
+  date.setAttribute("maxlength", 5);
+  date.title = "enter MMYY";
+  date.required = true;
+
+  formInputDate.append(dateLabel, date);
+
+  date.addEventListener("input", () => {
+    date.value = date.value.replace(/[^\d]/g, "");
+    date.value = mask(date.value, 2, "/");
+    cardDate.textContent = date.value;
+  });
+
+  // cvv код
+  const formInputCvv = document.createElement("div");
+  formInputCvv.classList.add("form__input-wrap", "form__input-wrap_cvv");
+
+  const cvvLabel = document.createElement("label");
+  cvvLabel.classList.add("form__label", "form__cvv-label");
+  cvvLabel.textContent = "CVV";
+
+  const cvv = document.createElement("input");
+  cvv.classList.add("input", "input__cvv");
+  cvv.type = "text";
+  cvv.name = "cvv";
+  cvv.pattern = /^\d{0,3}$/g;
+  cvv.setAttribute("maxlength", 3);
+  cvv.title = "enter 3 numbers only";
+  cvv.required = true;
+
+  formInputCvv.append(cvvLabel, cvv);
+
+  cvv.addEventListener("input", () => {
+    cvv.value = cvv.value.replace(/\D/g, "");
+  });
+
+  const formBtn = document.createElement("button");
+  formBtn.classList.add("form__button");
+  formBtn.textContent = "CHECK OUT";
+  formBtn.type = "submit";
+
+  formBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (holder.value.indexOf(" ") === -1) {
+      alert("Card Holder should comtain name and surname");
+      return;
+    }
+    const dateMMYY = date.value;
+    const dateYear = +dateMMYY.slice(3) + 2000;
+    const dateMonth = +dateMMYY.slice(0, 2);
+
+    if (dateMonth > 12) {
+      alert("The month (in date) should be from 01 to 12");
+      return;
+    }
+    let today = new Date();
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth() + 1;
+
+    if (
+      dateYear < currentYear ||
+      (dateYear === currentYear && dateMonth < currentMonth)
+    ) {
+      alert("Sorry, your card is expired");
+      return;
+    }
+    return;
+  });
+
+  form.append(
+    formInputHolder,
+    formInputNumber,
+    formInputDate,
+    formInputCvv,
+    formBtn
+  );
+
+  card.append(secure, creditCard, form);
+
+  wrapper.append(card);
+  document.body.append(wrapper);
+};
---
+++ script/index.js
@@ -0,0 +1,190 @@
+import { el, setChildren } from "../node_modules/redom/dist/redom.es.js";
+//import axios from "axios";
+
+
+
+const cardName = el("span", { className: "card__name" }, "John Doe");
+const cardNumber = el(
+  "span",
+  { className: "card__number" },
+  "xxxx xxxx xxxx xxxx"
+);
+const cardDate = el("span", { className: "card__date" }, "04/24");
+const CardRe = () => {
+  const creditCard = el(
+    "div",
+    { className: "credit-card" },
+    cardNumber,
+    el("div", { className: "card__personal" }, cardName, cardDate)
+  );
+
+  const mask = (value, limit, separator) => {
+    let output = [];
+    for (let i = 0; i < value.length; i++) {
+      if (i !== 0 && i % limit === 0) {
+        output.push(separator);
+      }
+      output.push(value[i]);
+    }
+    return output.join("");
+  };
+
+  const inputHolder = el("input", {
+    className: "input input__holder",
+    type: "text",
+    name: "holder",
+    maxlength: 27,
+    pattern: "[A-Za-zs]+$",
+    required: true,
+  });
+
+  function holderSubmit() {
+    inputHolder.addEventListener("input", () => {
+      inputHolder.value = inputHolder.value
+        .replace(/[^A-Za-z\s]+$/gi, "")
+        .toUpperCase();
+      cardName.textContent = inputHolder.value.toUpperCase();
+    });
+  }
+
+  const inputNumber = el("input", {
+    className: "input input__number",
+    type: "text",
+    name: "number",
+    pattern: /[^0-9]/gi,
+    maxlength: 19,
+    title: "enter 16 numbers",
+    required: true,
+  });
+
+  function numberSubmit() {
+    inputNumber.addEventListener("input", () => {
+      inputNumber.value = inputNumber.value.replace(/[^0-9]/gi, "");
+      inputNumber.value = mask(inputNumber.value, 4, " ");
+      cardNumber.textContent = inputNumber.value;
+    });
+  }
+
+  const inputDate = el("input", {
+    className: "input input__date",
+    type: "text",
+    name: "date",
+    pattern: /^\d{0,4}$/g,
+    maxlength: 5,
+    title: "enter MMYY",
+    required: true,
+  });
+
+  function dateSubmit() {
+    inputDate.addEventListener("input", () => {
+      inputDate.value = inputDate.value.replace(/[^\d]/g, "");
+      inputDate.value = mask(inputDate.value, 2, "/");
+      cardDate.textContent = inputDate.value;
+    });
+  }
+
+  const inputCvv = el("input", {
+    className: "input input__cvv",
+    type: "text",
+    name: "cvv",
+    pattern: /^\d{0,3}$/g,
+    maxlength: 3,
+    title: "enter 3 numbers only",
+    required: true,
+  });
+
+  function cvvSubmit() {
+    inputCvv.addEventListener("input", () => {
+      inputCvv.value = inputCvv.value.replace(/\D/g, "");
+    });
+  }
+
+  const formCard = el(
+    "form",
+    { className: "form" },
+    el(
+      "div",
+      { className: "form__input-wrap form__input-wrap_holder" },
+      el(
+        "label",
+        { className: "form__label form__holder-label" },
+        "Card Holder"
+      ),
+      inputHolder,
+      holderSubmit
+    ),
+    el(
+      "div",
+      { className: "form__input-wrap form__input-wrap_number" },
+      el(
+        "label",
+        { className: "form__label form__number-label" },
+        "Card Number"
+      ),
+      inputNumber,
+      numberSubmit
+    ),
+    el(
+      "div",
+      { className: "form__input-wrap form__input-wrap_date" },
+      el("label", { className: "form__label form__date-label" }, "Card Expiry"),
+      inputDate,
+      dateSubmit
+    ),
+    el(
+      "div",
+      { className: "form__input-wrap form__input-wrap_cvv" },
+      el("label", { className: "form__label form__cvv-label" }, "CVV"),
+      inputCvv,
+      cvvSubmit
+    ),
+    el(
+      "button",
+      { className: "form__button", type: "submit" },
+      formSubmit,
+      "CHECK OUT"
+    )
+  );
+
+  const card = el(
+    "div",
+    { className: "card" },
+    el("p", { className: "secure" }, "Secure Checkout"),
+    creditCard,
+    formCard
+  );
+
+  function formSubmit(el) {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (inputHolder.value.indexOf(" ") === -1) {
+        alert("Card Holder should comtain name and surname");
+        return;
+      }
+      const dateMMYY = inputDate.value;
+      const dateYear = +dateMMYY.slice(3) + 2000;
+      const dateMonth = +dateMMYY.slice(0, 2);
+
+      if (dateMonth > 12) {
+        alert("The month (in date) should be from 01 to 12");
+        return;
+      }
+      let today = new Date();
+      let currentYear = today.getFullYear();
+      let currentMonth = today.getMonth() + 1;
+
+      if (
+        dateYear < currentYear ||
+        (dateYear === currentYear && dateMonth < currentMonth)
+      ) {
+        alert("Sorry, your card is expired");
+        return;
+      }
+      alert("Success, you card is validated");
+      return;
+    });
+  }
+  return el("div", { className: "wrapper" }, card);
+};
+
+setChildren(document.body, CardRe());
